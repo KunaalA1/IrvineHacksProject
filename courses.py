0390bc7d9f7dcf16cde1a5f10c31d397f62b91f7
nlchau2004 <magegamingmc@gmail.com>
2024-01-26 20:18:26 -0800

    modified courses.py

--- courses.py
+++ courses.py
@@ -4,12 +4,4 @@ course_id = input()
 
 def parse_course_data(course_id:str):
     response = requests.get("https://api.peterportal.org/rest/v0/courses/" + course_id)
-    course = response.json()
-    return course["id"], course["prerequisite_for"]
-
-course, course_list = parse_course_data(course_id)
-print(course)
-
-for c in course_list:
-    course, course_list = parse_course_data(c)
-    print(course)
+    return response.json()
